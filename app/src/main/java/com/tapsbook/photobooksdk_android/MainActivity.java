e0f13659df09511c91928842e6527f03cb6e70f9
Li Ang <liangsjob@gmail.com>
2015-06-08 00:47:30 +0800

    fix dependencies and assets files

--- app/src/main/java/com/tapsbook/photobooksdk_android/MainActivity.java
+++ app/src/main/java/com/tapsbook/photobooksdk_android/MainActivity.java
@@ -1,13 +1,19 @@
 package com.tapsbook.photobooksdk_android;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Toast;
 
 import com.tapsbook.sdk.TapsbookSDK;
 import com.tapsbook.sdk.photos.Asset;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 
@@ -15,6 +21,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -26,9 +33,9 @@ public class MainActivity extends Activity {
 
     private ArrayList<Asset> exportPhotos() {
         ArrayList<Asset> assets = new ArrayList<>();
-        for (int i = 1; i < 23; i++) {
+        for (String file : getAssetsFiles()) {
             Asset asset = new Asset();
-            asset.originPath = "file:///android_asset/"+i+".jpg";
+            asset.originPath = getAssetsImageCacheFilePath(file);
             assets.add(asset);
         }
         return assets;
@@ -40,5 +47,39 @@ public class MainActivity extends Activity {
         } else {
             Toast.makeText(this, "Please make sure you have some favorite photos ", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private String getAssetsImageCacheFilePath(String file) {
+        if (file.endsWith("jpg")) {
+            File f = new File(getCacheDir() + file);
+            if (!f.exists()) {
+                try {
+                    InputStream is = getAssets().open(file);
+                    int size = is.available();
+                    byte[] buffer = new byte[size];
+                    is.read(buffer);
+                    is.close();
+
+                    FileOutputStream fos = new FileOutputStream(f);
+                    fos.write(buffer);
+                    fos.close();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return f.getPath();
+        }
+        return null;
+    }
+
+    private String[] getAssetsFiles() {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+
+        }
+        return files;
     }
 }

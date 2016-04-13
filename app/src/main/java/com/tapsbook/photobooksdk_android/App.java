3e80bb38585fc78240331c56190ed81e17e78c4d
xiao <xych@gmail.com>
2016-04-13 18:19:24 +0800

    update SDK and fix issue

--- app/src/main/java/com/tapsbook/photobooksdk_android/App.java
+++ app/src/main/java/com/tapsbook/photobooksdk_android/App.java
@@ -1,17 +1,36 @@
 package com.tapsbook.photobooksdk_android;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.tapsbook.sdk.TapsbookSDK;
+import com.tapsbook.sdk.TapsbookSDKCallback;
+import com.tapsbook.sdk.services.domain.LineItem;
+import com.tapsbook.sdk.utils.Utils;
 
-public class App extends MultiDexApplication {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class App extends MultiDexApplication implements TapsbookSDKCallback {
+
+    private static App instance;
+    public static App getInstance() {
+        return instance;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         TapsbookSDK.initialize("REPLACE_ME", this);
+        instance = this;
     }
 
     @Override
@@ -24,5 +43,33 @@ public class App extends MultiDexApplication {
     public void onTerminate() {
         super.onTerminate();
         TapsbookSDK.deinitialize();
+    }
+
+    @Override
+    public void complete(String s, LineItem lineItem) {
+        Toast.makeText(this, "saved /mnt/sdcard/Tapsbook/album.json", Toast.LENGTH_SHORT).show();
+        Gson gson = new Gson();
+        String json = gson.toJson(lineItem);
+        byte[] txt = json.getBytes();
+
+        try {
+            File path = new File("/mnt/sdcard/Tapsbook");
+            File f = new File("/mnt/sdcard/Tapsbook/album.json");
+            if (!path.exists()) {
+                path.mkdirs();
+            }
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+            OutputStream os = new FileOutputStream(f);
+            os.write(txt);
+            os.flush();
+            os.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
